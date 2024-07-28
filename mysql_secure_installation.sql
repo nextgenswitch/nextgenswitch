aaaf3f0249ff1b1be9730f852a28f536aa492750
Iftekhairul Alam <masum@infosoftbd.com>
2024-07-28 20:40:33 +0600

    Update mysql_secure_installation.sql

--- mysql_secure_installation.sql
+++ mysql_secure_installation.sql
@@ -1,4 +1,4 @@
-UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='nextgenswitch';
+UPDATE mysql.user SET Password=PASSWORD('nextgenswitch') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
