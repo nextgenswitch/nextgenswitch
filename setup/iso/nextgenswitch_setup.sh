96c111ad3cbae93fc2626a5a48ccaaa4c18b941d
Atik Hassan <atik@infosoftbd.com>
2025-05-28 16:20:14 +0600

    modify log message

--- setup/iso/nextgenswitch_setup.sh
+++ setup/iso/nextgenswitch_setup.sh
@@ -3,14 +3,14 @@
 
 
 # Do your actual work here
-echo "NextGenSwitch setup script running..." >> /var/log/nextgenswitch_setup.log
+echo "$(date '+%Y-%m-%d %H:%M:%S') - NextGenSwitch setup script running..." >> /var/log/nextgenswitch_setup.log
 mysql -sfu root < "/var/www/html/easypbx/setup/init.sql"
 mysql -ueasypbx -peasypbx easypbx < "/var/www/html/easypbx/setup/easypbx.sql"
 
 
 # Delete the service and script
-rm -f /etc/systemd/system/nextgenswitch_setup.service
-rm -- "$0"
+# rm -f /etc/systemd/system/nextgenswitch_setup.service
+# rm -- "$0"
 
 # Reload systemd
 systemctl daemon-reload
